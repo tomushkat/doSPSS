abd94af36ce948372c5a6f9beb4e5c09efe016ac
tomushkat <tmushkat@gmail.com>
2021-09-30 22:27:59 +0300

    Uploading again iqrOutliers instead of intervalOutliers

---
+++ R/iqrOutliers.R
@@ -0,0 +1,28 @@
+#' IQR Outliers
+#'
+#' This function takes a vector, and replace observations that are smaller or bigger than the lower/upper bounds with NA.
+#'
+#' @param data Input vector
+#' @param lowerBound Lower bound (default value is 2.5)
+#' @param upperBound Upper bound (default value is 2.5)
+#' @param replace If replace is TRUE (default is FALSE) than the function also use the replaceWith function with the median default values
+#'
+#'
+#' @return A vector with outliers as NA values
+#' @export
+#'
+#' @examples iqrOutliers(theData$Age)
+iqrOutliers <- function(data, lowerBound = 2.5, upperBound = 2.5, replace = FALSE){
+
+  Q      <- stats::quantile(data, probs = c(.25, .75), na.rm = TRUE)
+  iqr    <- stats::IQR(data, na.rm = TRUE)
+  High   <- Q[2] + upperBound * iqr
+  Low    <- Q[1] - lowerBound * iqr
+  Final  <- ifelse(data > High | data < Low, NA, data)
+
+  if(replace == TRUE){
+    Final <- replaceWith(Final, type = 'Median')
+  }
+
+  return(Final)
+}
