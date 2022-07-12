0366204224805a2e7acf7a9f98f05eb5de3cf917
Tom Mushkat <tmushkat@gmail.com>
2022-07-12 09:42:16 +0300

    TYPO

--- R/indttest.R
+++ R/indttest.R
@@ -44,25 +44,29 @@ indttest <- function(DV, IDV, Parametric = TRUE){
   EF <- "No effect size for insignificant results"
   trueVarTest <- 'No variation equality neaded of aparametric test'
 
-  if(Parametric == TRUE){
+  if (Parametric == TRUE) {
+
     varTest     <- stats::var.test(formula = DV ~ IDV, data = Data)
     trueVarTest <- ifelse(varTest$p.value < 0.05, FALSE, TRUE)
     Model       <- stats::t.test(formula = DV ~ IDV, data = Data,
-                                 var.equal = trueVarTest, alternative = "two.sided", mu = 0, paired = FALSE, conf.level = 0.95)
+                                 var.equal = trueVarTest, alternative = "two.sided",
+                                 mu = 0, paired = FALSE, conf.level = 0.95)
 
-    if(Model$p.value < 0.05){
+    if (Model$p.value < 0.05) {
 
       EF <- effectsize::effectsize(model = Model,
-                                   type = 'cohens_d', ci = .95, alternative = "two.sided")
+                                   type = 'cohens_d', ci = .95,
+                                   alternative = "two.sided")
     }
 
-  }else{
+  } else {
 
     Model <- stats::wilcox.test(formula = DV ~ IDV, data = Data,
-                                paired = FALSE, alternative = "two.sided", exact = NULL, mu = 0, correct = FALSE,
+                                paired = FALSE, alternative = "two.sided",
+                                exact = NULL, mu = 0, correct = FALSE,
                                 conf.int = FALSE, conf.level = 0.95)
 
-    if(Model$p.value < 0.05){
+    if (Model$p.value < 0.05) {
 
       EF <- effectsize::rank_epsilon_squared(DV ~ IDV, data = Data,
                                              mu = 0,
@@ -72,8 +76,6 @@ indttest <- function(DV, IDV, Parametric = TRUE){
     }
 
   }
-
-
 
   Data$IDV <- as.factor(Data$IDV)
   Figure <-
@@ -91,18 +93,26 @@ indttest <- function(DV, IDV, Parametric = TRUE){
   L <- list(Descriptive_statistics = Statistics, Model_summary = Model, Effect_size = EF, Variance_Equality = trueVarTest, Figure = Figure)
 
   freq <- table(IDV)
-  if(Parametric == TRUE & ((freq[1] | freq[2]) < 30)){
-    if(trueVarTest == FALSE){
+  if (Parametric == TRUE & ((freq[1] | freq[2]) < 30)) {
+
+    if (trueVarTest == FALSE) {
+
       lmModel <- stats::lm(formula = DV ~ IDV, data = Data)
-      Res <- lmModel$residuals
-    }else{
+      Res     <- lmModel$residuals
+
+    } else {
+
       lmModel <- estimatr::lm_robust(formula = DV ~ IDV, se_type = 'HC2', data = Data)
-      Res <- Data$DV - lmModel$fitted.values
+      Res     <- Data$DV - lmModel$fitted.values
+
     }
+
     shapiroTest <- stats::shapiro.test(Res)
-    if(shapiroTest$p.value < .05){
+
+    if (shapiroTest$p.value < .05) {
       Warning <- c("Warning: At list one of the groups has fewer than 30 observations, and the residuals' distribution is not normal. Considre to use an Mann-Whitney test (Parametric = FALSE)")
       print(Warning)
+
     }
   }
 
